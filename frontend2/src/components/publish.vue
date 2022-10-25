b38050f73b65461db92e70cf8168478a907a599f
Herve Colard <herve.colard@gmail.com>
2022-10-25 13:54:57 +0200

    add try

--- frontend2/src/components/publish.vue
+++ frontend2/src/components/publish.vue
@@ -1,78 +1,95 @@
 <template>
-  <input type="button" class="regular-button btn blue-bt logout-button" value="Déconnecter" id="logout" @click="logout()">
+  <input
+    type="button"
+    class="regular-button btn blue-bt logout-button"
+    value="Déconnecter"
+    id="logout"
+    @click="logout()"
+  />
   <div class="publishPost">
-      <div class="name-container postSpaces">
-        <p>Vous</p>
-      </div>
-      <div class="post-container postSpaces">
-        <h2>Écrivez quelque chose ou postez une image</h2>
-        <form action="" method="post" id="form">
-          <div class="textarea-container">
-            <textarea
-              name="title"
-              id="title"
-              class="textarea"
-              placeholder="Hey... écris quelquechose!"
-              v-model="title"
-            ></textarea>
-          </div>
-          <div class="bt-bar">
-            <div class="picture-bt-container">
-              <button class="regular-button picture-bt">
-                <input type="file" @change="previewImage" name="imageUrl" id="imageUrl" accept="image/*"/>
-                <img
-                  src="../../public/ICONE_PICTURE.png"
-                  alt="icone picture"
-                  class="icone-picture"
-                />
-              </button>
-              <div class="image-preview">
-                <img class="preview" :src="imageData">
-              </div>
-
-              <small>format accepté: png, jpg, jpeg</small>
-            </div>
-          </div>
-          <div class="bt-bar">
-            <div>
-              <input type="button" class="regular-button" id="annuler" @click="reload()" value="Annuler">
-            </div>
-            <div class="blue-bt">
-              <input
-                type="button"
-                value="Post"
-                id="post"
-                class="regular-button"
-                @click="sendPost()"
-              />
-            </div>
-          </div>
-
-        </form>
-      </div>
+    <div class="name-container postSpaces">
+      <p>Vous</p>
     </div>
+    <div class="post-container postSpaces">
+      <h2>Écrivez quelque chose ou postez une image</h2>
+      <form action="" method="post" id="form">
+        <div class="textarea-container">
+          <textarea
+            name="title"
+            id="title"
+            class="textarea"
+            placeholder="Hey... écris quelquechose!"
+            v-model="title"
+          ></textarea>
+        </div>
+        <div class="bt-bar">
+          <div class="picture-bt-container">
+            <button class="regular-button picture-bt">
+              <input
+                type="file"
+                @change="previewImage"
+                name="imageUrl"
+                id="imageUrl"
+                accept="image/*"
+              />
+              <img
+                src="../../public/ICONE_PICTURE.png"
+                alt="icone picture"
+                class="icone-picture"
+              />
+            </button>
+            <div class="image-preview">
+              <img class="preview" :src="imageData" />
+            </div>
+
+            <small>format accepté: png, jpg, jpeg</small>
+          </div>
+        </div>
+        <div class="bt-bar">
+          <div>
+            <input
+              type="button"
+              class="regular-button"
+              id="annuler"
+              @click="reload()"
+              value="Annuler"
+            />
+          </div>
+          <div class="blue-bt">
+            <input
+              type="button"
+              value="Post"
+              id="post"
+              class="regular-button"
+              @click="sendPost()"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-import { isTemplateNode } from '@vue/compiler-core';
+import { isTemplateNode } from "@vue/compiler-core";
 //import { buffer } from 'stream/consumers';
 // import axios from 'axios'
 //retrieve token
-var retrieveObject = localStorage.getItem('tokenObject');
-console.log('retrieveObject: ', JSON.parse(retrieveObject))
+var retrieveObject = localStorage.getItem("tokenObject");
+console.log("retrieveObject: ", JSON.parse(retrieveObject));
 var tokenObject = JSON.parse(retrieveObject);
 
 //retrieve firstName
-var firstName = localStorage.getItem('firstName');
-console.log(firstName)
+var firstName = localStorage.getItem("firstName");
+console.log(firstName);
 //retrieve lastName
-var lastName = localStorage.getItem('lastName');
-console.log(lastName)
+var lastName = localStorage.getItem("lastName");
+console.log(lastName);
 
 export default {
-  name: 'post',
-  data(){
-    return{
+  name: "post",
+  data() {
+    return {
       title: "",
       date: "",
       imageData: undefined,
@@ -82,8 +99,8 @@ export default {
       lastName: "",
       likes: 0,
       usersLiked: [],
-      selectedFile: null
-      }
+      selectedFile: null,
+    };
   },
   methods: {
     reload() {
@@ -106,61 +123,48 @@ export default {
         reader.readAsDataURL(input.files[0]);
       }
     },
-//     uploadImage() {
-//       const file = document.querySelector('input[type=file]').files[0]
-//       const reader = new FileReader()
+    //     uploadImage() {
+    //       const file = document.querySelector('input[type=file]').files[0]
+    //       const reader = new FileReader()
 
-//       const rawImg = reader.readAsDataURL(file)
-//       console.log(file)
-//       console.log(rawImg)
-// },
-    sendPost() {
+    //       const rawImg = reader.readAsDataURL(file)
+    //       console.log(file)
+    //       console.log(rawImg)
+    // },
+    async sendPost() {
+      try {
+        var option = {
+          title: this.title,
+          date: new Date(),
+          imageBase64: this.imageData,
+          // imageUrlLocal: JSON.stringify(image.files[0]),
+          userId: localStorage.getItem("user"),
+          firstName: localStorage.getItem("firstName"),
+          lastName: localStorage.getItem("lastName"),
+          likes: this.likes,
+          usersLiked: this.userLiked,
+        };
 
-      var image = document.querySelector('input[type="file"]')
-      var newDate = new Date();
-      var today = newDate.toLocaleDateString("fr");
+        let response = await fetch(`http://localhost:3000/api/posts`, {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${tokenObject.token}`,
+          },
+          body: JSON.stringify(option),
+        });
 
-      var option = {
-        title: this.title,
-        date: new Date(),
-        imageBase64: this.imageData,
-        // imageUrlLocal: JSON.stringify(image.files[0]),
-        userId: localStorage.getItem('user'),
-        firstName: localStorage.getItem('firstName'),
-        lastName: localStorage.getItem('lastName'),
-        likes: this.likes,
-        usersLiked: this.userLiked
-      };
-
-      fetch(`http://localhost:3000/api/posts`, {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-          'Authorization': `Bearer ${tokenObject.token}`
-        },
-        body: JSON.stringify(option),
-      })
-      .then(function(res) {
-        console.log(option);
-        console.log(res)
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then(function(res) {
-        console.log(res)
-        let response = res.body;
-
+        console.log(response);
         let postObject = response;
-        console.log(postObject)
 
         localStorage.setItem("post", postObject);
         let post = localStorage.getItem("post");
 
         // window.location.href = "/home";
-      })
-    }
-  }
-}
+      } catch (e) {
+        console.log("ERROR : ", e);
+      }
+    },
+  },
+};
 </script>
-
